a46e2f4eeab5b56ac26fd89d65fa022844f4dafa
Soomin <soomin3121@gmail.com>
2025-01-11 17:18:46 +0900

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@
 
 export const metadata = {
 	title: 'React Metadata Test',
-	description: 'A test page for React 19 metadata functionality.',
+	description: '리액트 19 메타데이터 기능을 테스트합니다.',
 	openGraph: {
 		title: 'React Metadata Test',
-		description: 'This is an example to test metadata in React 19.',
+		description: '리액트 메타데이터 테스트.',
 		url: 'react19-test.vercel.app', // Vercel 배포 후 업데이트
 		images: [
 			{
-				url: 'https://your-vercel-domain.vercel.app/og-image.png', // Vercel에 배포한 이미지 URL
+				url: 'https://react19-test.vercel.app/yeah.jpg', // Vercel에 배포한 이미지 URL
 				alt: 'React Metadata Example Image',
 			},
 		],

77f6bbe3bcf7fb00edc371c3fd6d4a1527647a37
Soomin <soomin3121@gmail.com>
2025-01-11 17:38:55 +0900

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
-'use client';
-
-export const metadata = {
-	title: 'React Metadata Test',
-	description: '리액트 19 메타데이터 기능을 테스트합니다.',
-	openGraph: {
-		title: 'React Metadata Test',
-		description: '리액트 메타데이터 테스트.',
-		url: 'react19-test.vercel.app', // Vercel 배포 후 업데이트
-		images: [
-			{
-				url: 'https://react19-test.vercel.app/yeah.jpg', // Vercel에 배포한 이미지 URL
-				alt: 'React Metadata Example Image',
-			},
-		],
-	},
-};
-
 function App() {
+	const metadata = {
+		title: 'React Metadata Test',
+    link: 'https://react19-test-oxx0lo5c7-soomin13s-projects.vercel.app/yeah.jpg'
+	};
 	return (
-		<div>
-			<h1>React Metadata Test</h1>
-			<p>This page demonstrates React 19 metadata functionality.</p>
-		</div>
+		<>
+			<h1>{metadata.title}</h1>
+			<title>{metadata.title}</title>
+			<meta name='soomin'/>
+			<div>
+				<h1>React Metadata Test</h1>
+				<p>This page demonstrates React 19 metadata functionality.</p>
+			</div>
+		</>
 	);
 }
 
